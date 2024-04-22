3d7cf133e09e725ce5cf45583b083528b1a70b40
Nerukki <pokengamer@gmail.com>
2024-04-22 07:57:12 -0300

    adicionando setter e getter

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+import br.com.alura.screenmatch.modelos.Movie;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        movie1.setTitle("O poderoso chefão");
+        movie1.setReleaseDate(1970);
+        movie1.setTimeInMinutes(180);
+
+
+        movie1.showDatasheet();
+
+        movie1.rate(8);
+        movie1.rate(5);
+        movie1.rate(10);
+
+
+        System.out.println("tatal de avaliações: " + movie1.getAmountOfRating());
+        System.out.println(movie1.getAvaregeRating());
+
+        //System.out.println(movie1.totalRating);
+        //System.out.println(movie1.amountOfRating);
+
+    }
 }
